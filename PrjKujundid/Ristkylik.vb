06649df02b145e5b7c5c90b4b1fd88470c0bdd3f
Uku-Erik Hindrikson <ukuerik.hindrikson@gmail.com>
2024-02-28 17:23:25 +0200

    ristkylik

---
+++ PrjKujundid/Ristkylik.vb
@@ -0,0 +1,10 @@
+﻿Public Class Ristkylik
+    Inherits Nelinurk
+
+    Public Sub New(ByVal alus As Double, ByVal korgus As Double)
+        MyBase.New("Ristkylik", alus, korgus, alus, korgus)
+    End Sub
+    Public Overrides Function leiaPindala() As Double
+        Return kylgA * kylgB
+    End Function
+End Class
