0aa1b3b60c363e9603f0783a212dedba276dee86
Uku-Erik Hindrikson <ukuerik.hindrikson@gmail.com>
2024-02-28 17:35:27 +0200

    gui

--- PrjKujundid/formKujundid.Designer.vb
+++ PrjKujundid/formKujundid.Designer.vb
@@ -22,17 +22,209 @@ Partial Class formKujundid
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtKylgA = New System.Windows.Forms.TextBox()
+        Me.txtKylgB = New System.Windows.Forms.TextBox()
+        Me.lblTyyp = New System.Windows.Forms.Label()
+        Me.txtYmbermoot = New System.Windows.Forms.TextBox()
+        Me.txtPindala = New System.Windows.Forms.TextBox()
+        Me.btnKolmnurk = New System.Windows.Forms.Button()
+        Me.btnRuut = New System.Windows.Forms.Button()
+        Me.btnRistkylik = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'txtKylgA
+        '
+        Me.txtKylgA.Location = New System.Drawing.Point(94, 21)
+        Me.txtKylgA.Name = "txtKylgA"
+        Me.txtKylgA.Size = New System.Drawing.Size(100, 22)
+        Me.txtKylgA.TabIndex = 0
+        '
+        'txtKylgB
+        '
+        Me.txtKylgB.Location = New System.Drawing.Point(94, 49)
+        Me.txtKylgB.Name = "txtKylgB"
+        Me.txtKylgB.Size = New System.Drawing.Size(100, 22)
+        Me.txtKylgB.TabIndex = 1
+        '
+        'lblTyyp
+        '
+        Me.lblTyyp.AutoSize = True
+        Me.lblTyyp.Location = New System.Drawing.Point(140, 51)
+        Me.lblTyyp.Name = "lblTyyp"
+        Me.lblTyyp.Size = New System.Drawing.Size(76, 16)
+        Me.lblTyyp.TabIndex = 2
+        Me.lblTyyp.Text = "maaramata"
+        '
+        'txtYmbermoot
+        '
+        Me.txtYmbermoot.Location = New System.Drawing.Point(143, 70)
+        Me.txtYmbermoot.Name = "txtYmbermoot"
+        Me.txtYmbermoot.Size = New System.Drawing.Size(100, 22)
+        Me.txtYmbermoot.TabIndex = 3
+        '
+        'txtPindala
+        '
+        Me.txtPindala.Location = New System.Drawing.Point(143, 97)
+        Me.txtPindala.Name = "txtPindala"
+        Me.txtPindala.Size = New System.Drawing.Size(100, 22)
+        Me.txtPindala.TabIndex = 4
+        '
+        'btnKolmnurk
+        '
+        Me.btnKolmnurk.Location = New System.Drawing.Point(68, 30)
+        Me.btnKolmnurk.Name = "btnKolmnurk"
+        Me.btnKolmnurk.Size = New System.Drawing.Size(75, 23)
+        Me.btnKolmnurk.TabIndex = 5
+        Me.btnKolmnurk.Text = "Kolmnurk"
+        Me.btnKolmnurk.UseVisualStyleBackColor = True
+        '
+        'btnRuut
+        '
+        Me.btnRuut.Location = New System.Drawing.Point(68, 60)
+        Me.btnRuut.Name = "btnRuut"
+        Me.btnRuut.Size = New System.Drawing.Size(75, 23)
+        Me.btnRuut.TabIndex = 6
+        Me.btnRuut.Text = "Ruut"
+        Me.btnRuut.UseVisualStyleBackColor = True
+        '
+        'btnRistkylik
+        '
+        Me.btnRistkylik.Location = New System.Drawing.Point(68, 90)
+        Me.btnRistkylik.Name = "btnRistkylik"
+        Me.btnRistkylik.Size = New System.Drawing.Size(75, 23)
+        Me.btnRistkylik.TabIndex = 7
+        Me.btnRistkylik.Text = "Ristkylik"
+        Me.btnRistkylik.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 16)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Kylg A"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 16)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Kylg B"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtKylgA)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtKylgB)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 20)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Algandmed"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnRuut)
+        Me.GroupBox2.Controls.Add(Me.btnKolmnurk)
+        Me.GroupBox2.Controls.Add(Me.btnRistkylik)
+        Me.GroupBox2.Location = New System.Drawing.Point(309, 20)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 134)
+        Me.GroupBox2.TabIndex = 11
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Kujundid"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.txtYmbermoot)
+        Me.GroupBox3.Controls.Add(Me.lblTyyp)
+        Me.GroupBox3.Controls.Add(Me.txtPindala)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 259)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(249, 179)
+        Me.GroupBox3.TabIndex = 12
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "GroupBox3"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 43)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 16)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Kujundi tyyp:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 70)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Ymbermoot"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 97)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Pindala"
         '
         'formKujundid
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "formKujundid"
         Me.Text = "Kujundid"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents txtKylgA As TextBox
+    Friend WithEvents txtKylgB As TextBox
+    Friend WithEvents lblTyyp As Label
+    Friend WithEvents txtYmbermoot As TextBox
+    Friend WithEvents txtPindala As TextBox
+    Friend WithEvents btnKolmnurk As Button
+    Friend WithEvents btnRuut As Button
+    Friend WithEvents btnRistkylik As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
 End Class
